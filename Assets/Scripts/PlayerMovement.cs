bf9369479200bb4bb76a8c5feda3cbbeaf316de9
Arkant04 <delacorte19@gmail.com>
2024-05-13 13:08:30 +0200

    Build 13/05/24: Punto 5 acabado

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,19 +11,78 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] ParticleSystem forwardParticles;
     [SerializeField] ParticleSystem rightParticles;
     [SerializeField] ParticleSystem leftParticles;
+    
+    Vector3 movement = Vector3.zero;
+
+    bool isForwardPlaying;
+    bool isRightPlaying;
+    bool isLeftPlaying;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        forwardParticles.Stop();
+        rightParticles.Stop();
+        leftParticles.Stop();
     }
 
     void Update()
     {
-        Vector3 movement = Vector3.zero;
+        MovementInput();
+    }
 
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if(movement.x > 0)
+        {
+            rb.velocity += transform.forward * stats.forwardAcceleration * Time.deltaTime;
+        }
+        else
+        {
+            //friction
+            rb.velocity = new Vector3(
+                    rb.velocity.x / (1 + stats.groundFriction * Time.deltaTime),
+                    rb.velocity.y,
+                    (rb.velocity.z / (1 + stats.groundFriction * Time.deltaTime)));
+            print("Toy dentro");
+            // Vector3 friction = -rb.velocity.normalized * stats.groundFriction;
+            // rb.velocity += friction * Time.deltaTime;
+        }
+
+        currentSpeed = rb.velocity.magnitude;
+
+        //velocidad maxima
+        if (currentSpeed >= stats.maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * stats.maxSpeed;
+            currentSpeed = stats.maxSpeed;
+        }
+    }
+
+    void MovementInput()
+    {
+        movement = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
         {
             movement.x += 1;
+            if (!isForwardPlaying)
+            {
+                forwardParticles.Play();
+                isForwardPlaying = true;
+            }
+        }
+        else if(Input.GetKeyUp(KeyCode.W))
+        {
+            if (isForwardPlaying)
+            {
+                forwardParticles.Stop();
+                isForwardPlaying = false;
+            }
         }
 
         if (Input.GetKey(KeyCode.S))
@@ -34,42 +93,46 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             rb.angularVelocity += rotate;
-            leftParticles.Play();
+            if (!isRightPlaying)
+            {
+                rightParticles.Play();
+                isRightPlaying = true;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            if (isRightPlaying)
+            {
+                rightParticles.Stop();
+                isRightPlaying = false;
+            }
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             rb.angularVelocity -= rotate;
-            rightParticles.Play();
+            if (!isLeftPlaying)
+            {
+                leftParticles.Play();
+                isLeftPlaying = true;
+            }
         }
-
-        else
+        else if (Input.GetKeyUp(KeyCode.A))
         {
-            forwardParticles.Stop();
-            leftParticles.Stop();
-            rightParticles.Stop();
+            if (isLeftPlaying)
+            {
+                leftParticles.Stop();
+                isLeftPlaying = false;
+            }
         }
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            forwardParticles.Play();
-        }
+        CheckParticles();
+    }
 
-        //friction
-        Vector3 friction = -rb.velocity.normalized * stats.groundFriction;
-        rb.velocity += friction * Time.deltaTime;
-
-
-        currentSpeed = rb.velocity.magnitude;
-
-        //velocidad maxima
-        if (currentSpeed > stats.maxSpeed)
-        {
-            rb.velocity = rb.velocity.normalized * stats.maxSpeed;
-            currentSpeed = stats.maxSpeed;
-        }
-
-        //aceleration
-        rb.velocity += movement * stats.forwardAcceleration * Time.deltaTime;
+    void CheckParticles()
+    {
+        isForwardPlaying = forwardParticles.isPlaying;
+        isRightPlaying = rightParticles.isPlaying;
+        isLeftPlaying = leftParticles.isPlaying;
     }
 }
